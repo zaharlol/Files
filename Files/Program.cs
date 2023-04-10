5df322e710b2587a7131ae7af25b38a504de43a5
zaharlol <kovtun.zahar12@mail.ru>
2023-04-10 17:47:01 +0300

    Добавьте файлы проекта.

---
+++ Files/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Runtime;
+
+class Drive
+{
+    public Drive(string name, long capacity, long freeplace)
+    {
+        Name = name;
+        Capacity = capacity;
+        FreePlace = freeplace;
+    }
+    public string Name { get; }
+    public long Capacity { get; }
+    public long FreePlace { get; }
+}
+public class Folder
+{
+    public List<string> Files { get; set; } = new List<string>();
+
+    Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+    public void CreateFolder(string name)
+    {
+        Folders.Add(name, new Folder());
+    }
+}
+
+namespace DriveManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"/" /* Или С:\\ для Windows */ );
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
+
+                DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
+
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                dirInfo.Delete(true);
+                newDirectory.Delete(true);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            static void Swap()
+            {
+                try
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(@"D:/VS/SkillFactory/testFolder");
+                    string trashPath = "/Users/denma/.Trash";
+
+                    dirInfo.MoveTo(trashPath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            
+        }
+    }
+}
---
+++ Классы/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+class FileWriter
+{
+    public static void Main()
+    {
+        string filePath = @"D:/VS/SkillFactory/Students.txt"; // Укажем путь 
+        if (!File.Exists(filePath)) // Проверим, существует ли файл по данному пути
+        {
+            //   Если не существует - создаём и записываем в строку
+            using (StreamWriter sw = File.CreateText(filePath))  // Конструкция Using (будет рассмотрена в последующих юнитах)
+            {
+                sw.WriteLine("Олег");
+                sw.WriteLine("Дмитрий");
+                sw.WriteLine("Иван");
+            }
+        }
+        // Откроем файл и прочитаем его содержимое
+        using (StreamReader sr = File.OpenText(filePath))
+        {
+            string str = "";
+            while ((str = sr.ReadLine()) != null) // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        string fileSK = @"D:/VS/Проекты/Files/Классы/Program.cs";
+        using (StreamReader sr = File.OpenText(fileSK))
+        {
+            string str = "";
+            while ((str = sr.ReadLine()) != null) // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        var fleInfo = new FileInfo("D:/VS/Проекты/Files/Классы/Program.cs");
+
+        using (StreamWriter sw = fleInfo.AppendText())
+        {
+            sw.WriteLine($"// Время запуска: {DateTime.Now}");
+        }
+
+        using (StreamReader sr = fleInfo.OpenText())
+        {
+            string str = "";
+            while ((str = sr.ReadLine()) != null)
+                Console.WriteLine(str);
+
+        }
+    }
+}
