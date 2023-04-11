9d0e99f7019eceddcd264676b40151fb94b21111
zaharlol <kovtun.zahar12@mail.ru>
2023-04-11 12:29:31 +0300

    Сериализация

--- Бинарные данныен/Program.cs
+++ Бинарные данныен/Program.cs
@@ -1,13 +1,40 @@
-﻿class Program
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         string StringValue;
         string Q = @"D:/VS/SkillFactory/BinaryFile.bin";
+
+        using (BinaryWriter writer = new BinaryWriter(File.Open(Q, FileMode.Open)))
+            writer.Write($"Дата: {DateTime.Now}, версия Windows {Environment.OSVersion}");
+
         using (BinaryReader reader = new BinaryReader(File.Open(Q, FileMode.Open)))
         {
             StringValue = reader.ReadString();
         }
         Console.WriteLine("Строка: " + StringValue);
+
+        var contact = new Contact("Voi", 8900250, "mail");
+        BinaryFormatter formatter = new BinaryFormatter();
+        using (var fr = new FileStream("Fert.dat", FileMode.OpenOrCreate))
+        {
+            formatter.Serialize(fr, contact);
+        }
+    }
+}
+[Serializable]
+class Contact
+{
+    public string Name { get; set; }
+    public long PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public Contact(string name, long phoneNumber, string email)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+        Email = email;
     }
 }
