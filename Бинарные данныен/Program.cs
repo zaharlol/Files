f56aa69cc786f360f870da3a59813c37ff13ed0b
zaharlol <kovtun.zahar12@mail.ru>
2023-04-11 11:50:51 +0300

    8,4,1

--- Бинарные данныен/Program.cs
+++ Бинарные данныен/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string StringValue;
+        string Q = @"D:/VS/SkillFactory/BinaryFile.bin";
+        using (BinaryReader reader = new BinaryReader(File.Open(Q, FileMode.Open)))
+        {
+            StringValue = reader.ReadString();
+        }
+        Console.WriteLine("Строка: " + StringValue);
+    }
+}
